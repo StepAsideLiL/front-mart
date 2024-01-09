6b9f4d56176303ba48501781bdd3816d812d0fc8
Rifat Khan <rifat6633@gmail.com>
2024-01-09 14:40:35 +0600

    add getProductById function to fetch a product from database

--- lib/data.ts
+++ lib/data.ts
@@ -10,3 +10,18 @@ export const getProducts = async () => {
     throw new Error("Could not fetch the products.");
   }
 };
+
+export const getProductById = async (id: string) => {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    return product;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Could not fetch the product by id.");
+  }
+};
