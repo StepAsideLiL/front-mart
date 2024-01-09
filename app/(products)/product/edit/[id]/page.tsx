1de9717a9bad4f70f30d6f52644409487cf79f34
Rifat Khan <rifat6633@gmail.com>
2024-01-09 12:07:12 +0600

    create edit product page

---
+++ app/(products)/product/edit/[id]/page.tsx
@@ -0,0 +1,16 @@
+import Main from "@/components/uis/main";
+import Title from "@/components/uis/title";
+
+const EditProductPage = () => {
+  return (
+    <Main variant={"xl"}>
+      <section>
+        <Title>Edit</Title>
+      </section>
+
+      <section>Edit</section>
+    </Main>
+  );
+};
+
+export default EditProductPage;
