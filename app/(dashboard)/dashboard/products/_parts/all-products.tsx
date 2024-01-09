9babafa76256062906d533e09df788518f594667
Rifat Khan <rifat6633@gmail.com>
2024-01-09 12:27:58 +0600

    remove fragment

--- app/(dashboard)/dashboard/products/_parts/all-products.tsx
+++ app/(dashboard)/dashboard/products/_parts/all-products.tsx
@@ -15,51 +15,49 @@ const AllProducts = async () => {
   const products = await getProducts();
 
   return (
-    <>
-      <ul className="space-y-3">
-        {products.map((product) => (
-          <Card key={product.id} className="grid grid-cols-12">
-            <CardHeader className="lg:col-span-6 col-span-7">
-              <CardTitle className="text-xl lg:text-2xl">
-                {product.title}
-              </CardTitle>
-              {product.quickOverview !== "" && (
-                <CardDescription>{product.quickOverview}</CardDescription>
-              )}
-            </CardHeader>
+    <ul className="space-y-3">
+      {products.map((product) => (
+        <Card key={product.id} className="grid grid-cols-12">
+          <CardHeader className="lg:col-span-6 col-span-7">
+            <CardTitle className="text-xl lg:text-2xl">
+              {product.title}
+            </CardTitle>
+            {product.quickOverview !== "" && (
+              <CardDescription>{product.quickOverview}</CardDescription>
+            )}
+          </CardHeader>
 
-            <CardContent className="lg:col-span-4 col-span-5 flex gap-2 p-0 py-6">
-              <div>
-                <h1 className={cn(product.discount !== 0 && "line-through")}>
-                  ${product.price}
-                </h1>
-                {product.discount !== 0 && <p>{product.discount}%</p>}
-              </div>
-
-              {product.discount !== 0 && (
-                <div>
-                  ${calculateDiscountedPrice(product.price, product.discount)}
-                </div>
-              )}
-            </CardContent>
-
-            <div className="lg:col-span-2 col-span-12 lg:p-6 pb-6 px-6 flex gap-2 lg:justify-end justify-start">
-              <Button size={"icon"} variant={"secondary"} asChild>
-                <Link href={`/product/edit/${product.id}`}>
-                  <span className="sr-only">Edit</span>
-                  <Edit />
-                </Link>
-              </Button>
-
-              <Button size={"icon"} variant={"destructive"}>
-                <span className="sr-only">Delete</span>
-                <Trash />
-              </Button>
+          <CardContent className="lg:col-span-4 col-span-5 flex gap-2 p-0 py-6">
+            <div>
+              <h1 className={cn(product.discount !== 0 && "line-through")}>
+                ${product.price}
+              </h1>
+              {product.discount !== 0 && <p>{product.discount}%</p>}
             </div>
-          </Card>
-        ))}
-      </ul>
-    </>
+
+            {product.discount !== 0 && (
+              <div>
+                ${calculateDiscountedPrice(product.price, product.discount)}
+              </div>
+            )}
+          </CardContent>
+
+          <div className="lg:col-span-2 col-span-12 lg:p-6 pb-6 px-6 flex gap-2 lg:justify-end justify-start">
+            <Button size={"icon"} variant={"secondary"} asChild>
+              <Link href={`/product/edit/${product.id}`}>
+                <span className="sr-only">Edit</span>
+                <Edit />
+              </Link>
+            </Button>
+
+            <Button size={"icon"} variant={"destructive"}>
+              <span className="sr-only">Delete</span>
+              <Trash />
+            </Button>
+          </div>
+        </Card>
+      ))}
+    </ul>
   );
 };
 
