87e4197558847542e3b54b7090b5bcca99833e01
Rifat Khan <rifat6633@gmail.com>
2024-01-09 19:03:23 +0600

    design quick view

--- app/(web)/shop/_parts/quick-view.tsx
+++ app/(web)/shop/_parts/quick-view.tsx
@@ -3,9 +3,14 @@ import {
   Sheet,
   SheetContent,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import CloudinaryImage from "@/components/uis/cloudinary-image";
+import Title from "@/components/uis/title";
+import { getProductById } from "@/lib/data";
+import { calculateDiscountedPrice, cn } from "@/lib/utils";
+import Link from "next/link";
+import { Suspense } from "react";
 
 const QuickView = ({ id }: { id: string }) => {
   return (
@@ -17,12 +22,66 @@ const QuickView = ({ id }: { id: string }) => {
         <Button className="rounded-3xl">Quick View</Button>
       </SheetTrigger>
 
-      <SheetContent className="space-y-2">
+      <SheetContent className="p-0 space-y-2 lg:max-w-xl lg:w-[576px] w-full overflow-auto">
         <SheetHeader>
-          <SheetTitle>Shopping Cart</SheetTitle>
+          <div className="h-4"></div>
         </SheetHeader>
+
+        <Suspense>
+          <Product id={id} />
+        </Suspense>
       </SheetContent>
     </Sheet>
+  );
+};
+
+const Product = async ({ id }: { id: string }) => {
+  const product = await getProductById(id);
+
+  return (
+    <div className="p-6 space-y-3">
+      <CloudinaryImage
+        src={product?.imageId || ""}
+        alt={`Photo of ${product?.title}`}
+      />
+
+      <Title variant={"xl2"}>
+        <Link href={`/shop/${id}`} className="hover:underline">
+          {product?.title}
+        </Link>
+      </Title>
+
+      <p className="text-sm">
+        <span
+          className={cn(
+            "text-base font-medium",
+            product!.discount !== 0 &&
+              "text-sm font-normal line-through text-muted-foreground"
+          )}
+        >
+          ${product?.price}
+        </span>{" "}
+        {product!.discount !== 0 && (
+          <>
+            <span className="text-base font-medium">
+              ${calculateDiscountedPrice(product!.price, product!.discount)}
+            </span>{" "}
+            <span className="text-red-500">{product!.discount}% off</span>
+          </>
+        )}
+      </p>
+
+      {product?.quickOverview && <p>{product?.quickOverview}</p>}
+
+      <Button className="w-fit inline-block">
+        Add to Cart $
+        {calculateDiscountedPrice(product!.price, product!.discount)}
+      </Button>
+
+      <Link href={`/shop/${id}`} className="hover:underline text-sm block">
+        View Full Product Details
+      </Link>
+    </div>
   );
 };
 
