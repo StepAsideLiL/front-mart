4038fc064077e15f2f605987939865c31bae18f3
Rifat Khan <rifat6633@gmail.com>
2024-01-09 11:53:27 +0600

    add calculateDiscountedPrice function to calculate discounted price of the product

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const calculateDiscountedPrice = (
+  originalPrice: number,
+  discountPercentage: number | null
+): number => {
+  const discountAmount =
+    (originalPrice * (discountPercentage !== null ? discountPercentage : 0)) /
+    100;
+  const discountedPrice = originalPrice - discountAmount;
+  return Number(discountedPrice.toFixed(2));
+};
