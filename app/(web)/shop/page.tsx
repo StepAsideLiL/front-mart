36493724c5572393ac254040241cb95bfe58e868
Rifat Khan <rifat6633@gmail.com>
2024-01-09 12:53:26 +0600

    add AllProduct

--- app/(web)/shop/page.tsx
+++ app/(web)/shop/page.tsx
@@ -1,8 +1,22 @@
 import Main from "@/components/uis/main";
-import React from "react";
+import Title from "@/components/uis/title";
+import React, { Suspense } from "react";
+import AllProduct from "./_parts/all-products";
 
 const ShopPage = () => {
-  return <Main>hello</Main>;
+  return (
+    <Main variant={"container"}>
+      <section>
+        <Title>Shop</Title>
+      </section>
+
+      <section>
+        <Suspense fallback={"loading..."}>
+          <AllProduct />
+        </Suspense>
+      </section>
+    </Main>
+  );
 };
 
 export default ShopPage;
