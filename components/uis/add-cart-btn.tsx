9f4ab0c68ad9da42afa53d5e7effb82f31e85401
Rifat Khan <rifat6633@gmail.com>
2024-01-10 20:20:42 +0600

    update cart store on AddCartBtn click

--- components/uis/add-cart-btn.tsx
+++ components/uis/add-cart-btn.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { parseJson, stringifyJson } from "@/lib/local-storage";
+import { useCartStore } from "@/lib/store/cart-store";
 import { cn } from "@/lib/utils";
 
 const AddCartBtn = ({
@@ -19,6 +20,8 @@ const AddCartBtn = ({
   };
   className?: string;
 }) => {
+  const setCartCount = useCartStore((s) => s.setCartCount);
+
   const addToCart = () => {
     if (typeof window !== "undefined" && window.localStorage) {
       const localCartStr = localStorage.getItem("cart");
@@ -31,6 +34,7 @@ const AddCartBtn = ({
         if (!localCart.some((obj) => obj.id === cartInfo.id)) {
           localCart.push(cartInfo);
           localStorage.setItem("cart", stringifyJson(localCart));
+          setCartCount(localCart.length);
         }
       }
     }
