85cd8cc5f5b4db0b59a5902854b15eea18ed8e33
Rifat Khan <rifat6633@gmail.com>
2024-01-12 19:37:48 +0600

    add calculateCartPrice function to calculate cart price in checkout page

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prismadb";
+import { calculateDiscountedPrice } from "./utils";
 
 export const getProducts = async () => {
   try {
@@ -23,5 +24,33 @@ export const getProductById = async (id: string) => {
   } catch (err) {
     console.log(err);
     throw new Error("Could not fetch the product by id.");
+  }
+};
+
+// Calacute the total price of the cart products on the chcckout page.
+export const calculateCartPrice = async (products: { id: string }[]) => {
+  try {
+    let price = 0;
+    await Promise.all(
+      products.map(async (item) => {
+        const product = await prisma.product.findUnique({
+          where: {
+            id: item.id,
+          },
+        });
+
+        if (product!.discount === 0) {
+          price = price + product!.price;
+        } else {
+          price =
+            price + calculateDiscountedPrice(product!.price, product!.discount);
+        }
+      })
+    );
+
+    return price;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Could not calculate total price.");
   }
 };
