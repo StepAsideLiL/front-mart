1a5487f3258a4b5a6de5b72ad36f0cded8285ffb
Rifat Khan <rifat6633@gmail.com>
2024-01-12 19:36:32 +0600

    add totol price section in checkout page

--- app/(web)/checkout/page.tsx
+++ app/(web)/checkout/page.tsx
@@ -2,6 +2,8 @@ import Main from "@/components/uis/main";
 import Title from "@/components/uis/title";
 import { Suspense } from "react";
 import ProductCard from "./_parts/product-cart";
+import TotalPrice from "./_parts/total-price";
+import { Separator } from "@/components/ui/separator";
 
 type ProductCart = {
   id: string;
@@ -13,17 +15,31 @@ const CheckoutPage = ({ searchParams }: { searchParams: { cart: string } }) => {
   return (
     <Main variant={"container"}>
       <section>
-        <Title>Checkout</Title>
+        <Title variant={"xl2"}>Checkout</Title>
       </section>
 
-      <section>
-        <Suspense fallback={"loading..."}>
-          <div className="space-y-5">
+      <section className="w-full flex flex-col md:flex-row gap-5 justify-between">
+        <section className="w-full space-y-5">
+          <Suspense fallback={"loading..."}>
             {products.map((product) => (
               <ProductCard key={product.id} id={product.id} />
             ))}
+          </Suspense>
+        </section>
+
+        <section className="w-full">
+          <h1 className="text-xl font-medium">Total</h1>
+
+          <div className="py-2">
+            <Separator orientation="horizontal" />
           </div>
-        </Suspense>
+
+          <section>
+            <Suspense fallback={"loading..."}>
+              <TotalPrice products={products} />
+            </Suspense>
+          </section>
+        </section>
       </section>
     </Main>
   );
