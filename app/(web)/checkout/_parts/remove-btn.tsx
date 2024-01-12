1dc43db502ca5648f0e59cfe7095413aa271fb00
Rifat Khan <rifat6633@gmail.com>
2024-01-12 12:11:31 +0600

    implement remove btn logic for check out page

--- app/(web)/checkout/_parts/remove-btn.tsx
+++ app/(web)/checkout/_parts/remove-btn.tsx
@@ -1,10 +1,26 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { redirect, useRouter, useSearchParams } from "next/navigation";
+
+type ProductCart = {
+  id: string;
+}[];
 
 const RemoveBtn = ({ id }: { id: string }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const products: ProductCart = JSON.parse(searchParams.get("cart") || "");
+
   const removeProduct = () => {
     console.log(id);
+    console.log(searchParams.get("cart"));
+
+    const newProducts = products.filter((product) => product.id !== id);
+    console.log(newProducts);
+
+    replace(`/checkout?cart=${JSON.stringify(newProducts)}`);
+    redirect(`/checkout?cart=${JSON.stringify(newProducts)}`);
   };
 
   return (
