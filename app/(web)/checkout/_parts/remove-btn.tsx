512f24430613dd1a438868d96aa40e3a0c4b1016
Rifat Khan <rifat6633@gmail.com>
2024-01-12 14:59:17 +0600

    remove cart items from local storage

--- app/(web)/checkout/_parts/remove-btn.tsx
+++ app/(web)/checkout/_parts/remove-btn.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { parseJson, stringifyJson } from "@/lib/local-storage";
+import { useCartStore } from "@/lib/store/cart-store";
 import { redirect, useRouter, useSearchParams } from "next/navigation";
 
 type ProductCart = {
@@ -10,20 +12,32 @@ type ProductCart = {
 const RemoveBtn = ({ id }: { id: string }) => {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
+  const setCartCount = useCartStore((s) => s.setCartCount);
   const products: ProductCart = JSON.parse(searchParams.get("cart") || "");
 
   const removeProduct = () => {
     const newProducts = products.filter((product) => product.id !== id);
-    replace(`/checkout?cart=${JSON.stringify(newProducts)}`);
-    if (newProducts.length === 0) {
-      redirect("/shop");
+
+    if (typeof window !== "undefined" && window.localStorage) {
+      const localCartStr = localStorage.getItem("cart") || "[]";
+      const localCart = parseJson(localCartStr);
+      const newLocalCart = localCart.filter((product) => product.id !== id);
+      localStorage.setItem("cart", stringifyJson(newLocalCart));
+      setCartCount(newLocalCart.length);
     }
-    redirect(`/checkout?cart=${JSON.stringify(newProducts)}`);
+
+    if (newProducts.length === 0) {
+      replace("/shop");
+      redirect("/shop");
+    } else {
+      replace(`/checkout?cart=${JSON.stringify(newProducts)}`);
+      redirect(`/checkout?cart=${JSON.stringify(newProducts)}`);
+    }
   };
 
   return (
     <Button
-      variant={"outline"}
+      variant={"destructive"}
       className="w-fit"
       onClick={() => removeProduct()}
     >
