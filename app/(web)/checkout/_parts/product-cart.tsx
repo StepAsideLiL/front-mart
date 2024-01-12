f9082f94e5df1ee413892c2dd3d143f976d4cb16
Rifat Khan <rifat6633@gmail.com>
2024-01-12 11:17:59 +0600

    cart details in checkout page

---
+++ app/(web)/checkout/_parts/product-cart.tsx
@@ -0,0 +1,46 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import CloudinaryImage from "@/components/uis/cloudinary-image";
+import { getProductById } from "@/lib/data";
+import { calculateDiscountedPrice, cn } from "@/lib/utils";
+
+const ProductCard = async ({ id }: { id: string }) => {
+  const product = await getProductById(id);
+
+  return (
+    <Card className="grid grid-cols-12 gap-2 max-w-md">
+      <CloudinaryImage
+        src={product?.imageId || ""}
+        alt={`Photo of ${product?.title}`}
+        crop="fill"
+        className="col-span-4"
+      />
+
+      <CardHeader className="col-span-8 p-0 py-2">
+        <CardTitle className="text-lg">{product?.title}</CardTitle>
+        <div>
+          <span
+            className={cn(
+              "text-base font-medium",
+              product!.discount !== 0 &&
+                "text-sm font-normal line-through text-muted-foreground"
+            )}
+          >
+            ${product?.price}
+          </span>{" "}
+          {product!.discount !== 0 && (
+            <>
+              <span className="text-base font-medium">
+                ${calculateDiscountedPrice(product!.price, product!.discount)}
+              </span>{" "}
+              <span className="text-red-500">{product!.discount}% off</span>
+            </>
+          )}
+        </div>
+
+        <div>hello</div>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default ProductCard;
