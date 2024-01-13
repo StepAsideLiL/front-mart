8064d1fad9302e1baba9895607cb6f55f010c3c4
Rifat Khan <rifat6633@gmail.com>
2024-01-13 16:43:38 +0600

    complete the design

--- app/(web)/cart/_parts/total-price.tsx
+++ app/(web)/cart/_parts/total-price.tsx
@@ -1,5 +1,5 @@
-import { calculateCartPrice, getProductById } from "@/lib/data";
-// import { calculateDiscountedPrice } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { calculateCartPrice } from "@/lib/data";
 
 const TotalPrice = async ({
   products,
@@ -9,8 +9,29 @@ const TotalPrice = async ({
   }[];
 }) => {
   const price = await calculateCartPrice(products);
+  const delivaryCharge = 25;
 
-  return <div>{price}</div>;
+  return (
+    <div className="p-10 pt-2">
+      <div className="flex justify-between">
+        <span className="text-sm text-muted-foreground">Sub total</span>{" "}
+        <span>${price.toFixed(2)}</span>
+      </div>
+      <div className="flex justify-between">
+        <span className="text-sm text-muted-foreground">Delevary charge</span>
+        <span>${delivaryCharge.toFixed(2)}</span>
+      </div>
+
+      <div className="py-2">
+        <Separator orientation="horizontal" />
+      </div>
+
+      <div className="flex justify-between text-lg">
+        <span>Total</span>
+        <span>${(price + delivaryCharge).toFixed(2)}</span>
+      </div>
+    </div>
+  );
 };
 
 export default TotalPrice;
