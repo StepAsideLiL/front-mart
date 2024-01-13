dc9659cb868a0c74aeeac659d05a4c65205afd24
Rifat Khan <rifat6633@gmail.com>
2024-01-13 19:33:11 +0600

    create checkout page and total price section

---
+++ app/(web)/checkout/_parts/total-price.tsx
@@ -0,0 +1,39 @@
+import { Separator } from "@/components/ui/separator";
+import { calculateCartPrice } from "@/lib/data";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const TotalPrice = async ({
+  products,
+}: {
+  products: {
+    id: string;
+  }[];
+}) => {
+  const price = await calculateCartPrice(products);
+  const delivaryCharge = 25;
+
+  return (
+    <div className="p-10 pt-2">
+      <div className="flex justify-between">
+        <span className="text-sm text-muted-foreground">Sub total</span>{" "}
+        <span>${price.toFixed(2)}</span>
+      </div>
+      <div className="flex justify-between">
+        <span className="text-sm text-muted-foreground">Delevary charge</span>
+        <span>${delivaryCharge.toFixed(2)}</span>
+      </div>
+
+      <div className="py-2">
+        <Separator orientation="horizontal" />
+      </div>
+
+      <div className="flex justify-between text-lg">
+        <span>Total</span>
+        <span>${(price + delivaryCharge).toFixed(2)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default TotalPrice;
---
+++ app/(web)/checkout/page.tsx
@@ -0,0 +1,40 @@
+import Main from "@/components/uis/main";
+import { Suspense } from "react";
+import { ProductCart } from "@/lib/types";
+import TotalPrice from "./_parts/total-price";
+import Title from "@/components/uis/title";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
+
+const CheckoutPage = ({ searchParams }: { searchParams: { cart: string } }) => {
+  const products: ProductCart = JSON.parse(searchParams.cart);
+
+  return (
+    <Main variant={"container"}>
+      <section>
+        <Title variant={"xl2"}>Checkout</Title>
+      </section>
+
+      <section className="w-full flex flex-col md:flex-row gap-5 justify-between">
+        <section className="w-full">form</section>
+
+        <section className="w-full">
+          <section className="flex items-center gap-2">
+            <h1 className="text-xl font-medium">Total</h1>
+            <Badge variant={"outline"}>{products.length}</Badge>
+          </section>
+
+          <div className="py-2">
+            <Separator orientation="horizontal" />
+          </div>
+
+          <Suspense fallback={"loading..."}>
+            <TotalPrice products={products} />
+          </Suspense>
+        </section>
+      </section>
+    </Main>
+  );
+};
+
+export default CheckoutPage;
