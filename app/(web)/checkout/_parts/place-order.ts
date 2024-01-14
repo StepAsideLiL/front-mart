f9dedb05251c52b2f360907a3740221f3891c3fb
Rifat Khan <rifat6633@gmail.com>
2024-01-14 11:53:24 +0600

    checkout form server action to place order and rediract to order page

---
+++ app/(web)/checkout/_parts/place-order.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import prisma from "@/lib/prismadb";
+import { PlaceOrder } from "@/lib/types";
+import { redirect } from "next/navigation";
+
+export const placeOrder = async (data: PlaceOrder) => {
+  const { name, email, address, zipCode, city, country, products } = data;
+
+  let orderId: string = "";
+
+  try {
+    const order = await prisma.order.create({
+      data: {
+        name: name,
+        email: email,
+        address: address,
+        zipCode: zipCode,
+        city: city,
+        country: country,
+        products: { hello: "wordl" },
+      },
+    });
+
+    orderId = order.id;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Could not create the order.");
+  }
+  redirect(`/order/${orderId}`);
+};
