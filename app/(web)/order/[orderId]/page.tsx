e6a3db6d022d534642eef5c397e85686aeb87a97
Rifat Khan <rifat6633@gmail.com>
2024-01-14 11:54:50 +0600

    create oder page and orderId dynamic page

---
+++ app/(web)/order/[orderId]/page.tsx
@@ -0,0 +1,5 @@
+const OrderIdPage = ({ params }: { params: { orderId: string } }) => {
+  return <div>OrderIdPage {params.orderId}</div>;
+};
+
+export default OrderIdPage;
---
+++ app/(web)/order/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+const OderPage = () => {
+  redirect("/shop");
+};
+
+export default OderPage;
