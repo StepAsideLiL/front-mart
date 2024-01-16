83cdb5fedb1a540c67400a93120a45e74bf18da8
Rifat Khan <rifat6633@gmail.com>
2024-01-16 09:59:41 +0600

    create ProductInfo

---
+++ app/(products)/product/edit/[productId]/_parts/product-info.tsx
@@ -0,0 +1,5 @@
+const ProductInfo = ({ productId }: { productId: string }) => {
+  return <div>{productId}</div>;
+};
+
+export default ProductInfo;
--- app/(products)/product/edit/[productId]/page.tsx
+++ app/(products)/product/edit/[productId]/page.tsx
@@ -1,14 +1,20 @@
 import Main from "@/components/uis/main";
 import Title from "@/components/uis/title";
+import ProductInfo from "./_parts/product-info";
+import { Suspense } from "react";
 
-const EditProductPage = () => {
+const EditProductPage = ({ params }: { params: { productId: string } }) => {
   return (
     <Main variant={"xl"}>
       <section>
-        <Title variant={"xl2"}>Edit</Title>
+        <Title variant={"xl2"}>Edit Product: {params.productId}</Title>
       </section>
 
-      <section>Edit</section>
+      <section>
+        <Suspense fallback={"loading..."}>
+          <ProductInfo productId={params.productId} />
+        </Suspense>
+      </section>
     </Main>
   );
 };
