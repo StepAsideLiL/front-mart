72beaad8744d40fa933604e3bf9eb4e649db5e70
Rifat Khan <rifat6633@gmail.com>
2024-01-16 13:00:58 +0600

    add deleteProduct function

--- app/(products)/product/edit/[productId]/_parts/actions.ts
+++ app/(products)/product/edit/[productId]/_parts/actions.ts
@@ -5,6 +5,7 @@ import { UpdateProductFormData } from "@/lib/types";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+// Update product info in the database.
 export const updateProduct = async (data: UpdateProductFormData) => {
   const {
     id,
@@ -35,6 +36,23 @@ export const updateProduct = async (data: UpdateProductFormData) => {
   } catch (err) {
     console.log(err);
     throw new Error("Failed to update product in database.");
+  }
+
+  revalidatePath("/dashboard/products");
+  redirect("/dashboard/products");
+};
+
+// Delete product from the database.
+export const deleteProduct = async (productId: string) => {
+  try {
+    await prisma.product.delete({
+      where: {
+        id: productId,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to delete product in database.");
   }
 
   revalidatePath("/dashboard/products");
