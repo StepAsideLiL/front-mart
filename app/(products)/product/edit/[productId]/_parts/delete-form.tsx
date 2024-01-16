61599c346b08c3a831187c48df70820cc7e6dbdd
Rifat Khan <rifat6633@gmail.com>
2024-01-16 13:04:04 +0600

    add remove button and form on responsive dialog box

---
+++ app/(products)/product/edit/[productId]/_parts/delete-form.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { deleteProduct } from "./actions";
+
+const formSchema = z.object({
+  keyword: z.string().min(1, { message: "Field can not be empty." }),
+});
+
+const DeleteForm = ({ productId }: { productId: string }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      keyword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (values.keyword === "delete") {
+      deleteProduct(productId);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="keyword"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="type..." {...field} autoComplete="off" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" variant={"destructive"}>
+          Remove
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default DeleteForm;
