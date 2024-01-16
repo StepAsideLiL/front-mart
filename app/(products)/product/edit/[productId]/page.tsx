ce456f45ce8a5fdeec013040f1fa6a13fe4920fa
Rifat Khan <rifat6633@gmail.com>
2024-01-16 10:39:22 +0600

    add delete button on product id dynamic page

--- app/(products)/product/edit/[productId]/page.tsx
+++ app/(products)/product/edit/[productId]/page.tsx
@@ -2,6 +2,7 @@ import Main from "@/components/uis/main";
 import Title from "@/components/uis/title";
 import ProductInfo from "./_parts/product-info";
 import { Suspense } from "react";
+import RemoveProduct from "./_parts/remove-product";
 
 const EditProductPage = ({ params }: { params: { productId: string } }) => {
   return (
@@ -14,6 +15,13 @@ const EditProductPage = ({ params }: { params: { productId: string } }) => {
         <Suspense fallback={"loading..."}>
           <ProductInfo productId={params.productId} />
         </Suspense>
+      </section>
+
+      <section className="p-10 border space-y-10">
+        <p className="text-destructive font-medium">
+          Remove product from the database
+        </p>
+        <RemoveProduct productId={params.productId} />
       </section>
     </Main>
   );
