095462ae4823032ce73f1eac06455998e949568d
Rifat Khan <rifat6633@gmail.com>
2024-01-17 11:27:50 +0600

    refactor design and add Discount and Price (After discount) columns

--- app/(dashboard)/dashboard/products/_parts/all-products-table.tsx
+++ app/(dashboard)/dashboard/products/_parts/all-products-table.tsx
@@ -23,6 +23,8 @@ const AllProductsTabel = async () => {
           <TableHead className="w-14"></TableHead>
           <TableHead>Title</TableHead>
           <TableHead>Price</TableHead>
+          <TableHead>Discount</TableHead>
+          <TableHead>Price (after discount)</TableHead>
           <TableHead></TableHead>
         </TableRow>
       </TableHeader>
@@ -36,29 +38,15 @@ const AllProductsTabel = async () => {
                 alt={`Photo of the ${product!.title}`}
               />
             </TableCell>
-            <TableCell>{product!.title}</TableCell>
             <TableCell>
-              <span
-                className={cn(
-                  "text-base font-medium",
-                  product!.discount !== 0 &&
-                    "text-sm font-normal line-through text-muted-foreground"
-                )}
-              >
-                ${product?.price}
-              </span>{" "}
-              {product!.discount !== 0 && (
-                <>
-                  <span className="text-base font-medium">
-                    $
-                    {calculateDiscountedPrice(
-                      product!.price,
-                      product!.discount
-                    )}
-                  </span>{" "}
-                  <span className="text-red-500">{product!.discount}% off</span>
-                </>
-              )}
+              <Link href={`/shop/${product!.id}`} className="hover:underline">
+                {product!.title}
+              </Link>
+            </TableCell>
+            <TableCell>${product!.price}</TableCell>
+            <TableCell>{product!.discount}%</TableCell>
+            <TableCell>
+              ${calculateDiscountedPrice(product!.price, product!.discount)}
             </TableCell>
             <TableCell className="text-right">
               <Button size={"icon"} variant={"secondary"} asChild>
