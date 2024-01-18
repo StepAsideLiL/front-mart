b08dbed257ad3f3f39c6839eae6535b0b26bc1cb
Rifat Khan <rifat6633@gmail.com>
2024-01-18 14:54:50 +0600

    add title metadata

--- app/(web)/shop/page.tsx
+++ app/(web)/shop/page.tsx
@@ -2,8 +2,13 @@ import Main from "@/components/uis/main";
 import Title from "@/components/uis/title";
 import { Suspense } from "react";
 import AllProduct from "./_parts/all-products";
+import { Metadata } from "next";
 
 export const revalidate = 600;
+
+export const metadata: Metadata = {
+  title: "Shop",
+};
 
 const ShopPage = () => {
   return (
