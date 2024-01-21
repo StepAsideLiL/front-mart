321ce2b8b3128e8d4c0cfb724358974d336ff48f
Rifat Khan <rifat6633@gmail.com>
2024-01-21 10:40:10 +0600

    add user profile menu

--- components/templates/avatar-menu.tsx
+++ components/templates/avatar-menu.tsx
@@ -8,7 +8,29 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { useState } from "react";
-import UserAvatar from "../uis/user-avatar";
+import UserAvatar from "@/components/uis/user-avatar";
+import { Gift, LayoutDashboard, ListOrdered } from "lucide-react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Separator } from "@radix-ui/react-separator";
+
+const menus1 = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: <LayoutDashboard strokeWidth={"1px"} width={"20px"} />,
+  },
+  {
+    title: "Products",
+    href: "/dashboard/products",
+    icon: <Gift strokeWidth={"1px"} width={"20px"} />,
+  },
+  {
+    title: "Orders",
+    href: "/dashboard/orders",
+    icon: <ListOrdered strokeWidth={"1px"} width={"20px"} />,
+  },
+];
 
 const AvatarMenu = () => {
   const [open, setOpen] = useState(false);
@@ -33,9 +55,35 @@ const AvatarMenu = () => {
           <SheetDescription>bio</SheetDescription>
         </SheetHeader>
 
-        <div></div>
+        <nav className="p-1 flex-col gap-1 flex">
+          {menus1.map((menu) => (
+            <ActiveLink key={menu.href} href={menu.href}>
+              <span>{menu.icon}</span>
+              <span>{menu.title}</span>
+            </ActiveLink>
+          ))}
+
+          <Separator orientation="horizontal" />
+        </nav>
       </SheetContent>
     </Sheet>
+  );
+};
+
+const ActiveLink = ({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href: string;
+}) => {
+  return (
+    <Link
+      href={href}
+      className={cn("flex gap-2 p-2 hover:bg-accent rounded items-center")}
+    >
+      {children}
+    </Link>
   );
 };
 
