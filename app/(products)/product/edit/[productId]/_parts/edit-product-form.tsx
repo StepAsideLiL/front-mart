760f0b0adaf54f519905516ba76956a75bfb8fe8
Rifat Khan <rifat6633@gmail.com>
2024-01-30 22:30:03 +0600

    udpate: edit product form and action

--- app/(products)/product/edit/[productId]/_parts/edit-product-form.tsx
+++ app/(products)/product/edit/[productId]/_parts/edit-product-form.tsx
@@ -28,7 +28,10 @@ const formSchema = z.object({
     .string()
     .min(1, { message: "Product title can not be empty." }),
   price: z.string().min(1, { message: "Product price can not be empty." }),
-  imageId: z.string().min(1, { message: "Set a product image" }),
+  imageSrc: z
+    .string()
+    .min(1, { message: "Set a product image" })
+    .url({ message: "Insert correct image url" }),
   discount: z.string().optional(),
   quickOverview: z.string().optional(),
   description: z.string().optional(),
@@ -51,9 +54,8 @@ const EditProductForm = ({
   } | null;
 }) => {
   const [isLoading, setIsLoading] = useState(false);
-
   const [imageSrc, setIamgeSrc] = useState(product?.imageSrc);
-  const [imageId, setIamgeId] = useState(product?.imageId);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -67,7 +69,7 @@ const EditProductForm = ({
     values: {
       productTitle: product?.title ? product?.title : "",
       price: product?.price ? product?.price.toString() : "",
-      imageId: imageId ? imageId : "",
+      imageSrc: imageSrc ? imageSrc : "",
     },
   });
 
@@ -80,18 +82,17 @@ const EditProductForm = ({
         parseFloat(values.discount ? values.discount : "0").toFixed(2)
       ),
       imageSrc: imageSrc ? imageSrc : "",
-      imageId: imageId ? imageId : "",
     };
-    setIsLoading(true);
 
+    setIsLoading(true);
     updateProduct(formData);
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <section className="flex flex-col-reverse md:flex-row gap-5 w-full">
-          <div className="w-full space-y-3">
+        <section className="w-full flex flex-col lg:flex-row gap-5">
+          <section className="w-full space-y-8">
             {/* Product Title */}
             <FormField
               control={form.control}
@@ -100,7 +101,11 @@ const EditProductForm = ({
                 <FormItem>
                   <FormLabel>Product Title</FormLabel>
                   <FormControl>
-                    <Input placeholder="Product" {...field} />
+                    <Input
+                      placeholder="Product"
+                      {...field}
+                      autoComplete="off"
+                    />
                   </FormControl>
                   <FormDescription>
                     Put appropriate title for the product.
@@ -118,7 +123,12 @@ const EditProductForm = ({
                 <FormItem>
                   <FormLabel>Product Price</FormLabel>
                   <FormControl>
-                    <Input type="number" placeholder="$3.99" {...field} />
+                    <Input
+                      type="number"
+                      placeholder="$3.99"
+                      {...field}
+                      autoComplete="off"
+                    />
                   </FormControl>
                   <FormDescription>Price of the product.</FormDescription>
                   <FormMessage />
@@ -126,170 +136,118 @@ const EditProductForm = ({
               )}
             />
 
-            {/* Product Quick Overview */}
+            {/* Product Image */}
             <FormField
               control={form.control}
-              name="quickOverview"
+              name="imageSrc"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Quick Overview (Optional)</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Write something short about the product..."
-                      {...field}
-                      rows={5}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    Write short description about the product.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            {/* Product Discount */}
-            <FormField
-              control={form.control}
-              name="discount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Discount (Optional)</FormLabel>
+                  <FormLabel>Product Image</FormLabel>
                   <FormControl>
                     <Input
-                      type="number"
-                      placeholder="40%"
+                      type="text"
+                      placeholder="https://example.com/prduct.jpg"
                       {...field}
-                      max={100}
+                      autoComplete="off"
+                      onChangeCapture={(e) => {
+                        setIamgeSrc(e.currentTarget.value);
+                      }}
                     />
                   </FormControl>
                   <FormDescription>
-                    Product discount amount in percentage.
+                    Valid image url of the product.
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
+          </section>
 
-            {/* Product Description */}
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Description (Optional)</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Write something about the product..."
-                      {...field}
-                      rows={10}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    Write useful description about the product.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="w-full space-y-3">
-            {/* Product Image */}
-            <div className="flex flex-col items-center gap-2">
-              <FormField
-                control={form.control}
-                name="imageId"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel className="w-full flex justify-between items-center gap-2">
-                      <p className="text-sm font-medium">Product Image</p>
-
-                      <CldUploadWidget
-                        uploadPreset="qaljd9iw"
-                        options={{
-                          sources: ["local", "url"],
-                        }}
-                        // @ts-expect-error
-                        onSuccess={(result: CloundinayImage, { widget }) => {
-                          setIamgeId(result.info.public_id);
-                          setIamgeSrc(result.info.secure_url);
-                          widget.close();
-                        }}
-                      >
-                        {({ open }) => {
-                          return (
-                            <Button
-                              variant={"secondary"}
-                              type="button"
-                              onClick={() => open()}
-                            >
-                              {imageId === ""
-                                ? "Upload a product image"
-                                : "Change image"}
-                            </Button>
-                          );
-                        }}
-                      </CldUploadWidget>
-                    </FormLabel>
-                    <FormControl>
-                      <Input type="hidden" placeholder="Product" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Put appropriate title for the product.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
+          <section className="w-full flex flex-col items-center gap-2">
+            <div className="w-72 h-72">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={imageSrc || `/images/placeholder-image.webp`}
+                alt="Product image"
+                className="w-full h-full object-cover"
               />
-
-              {imageId && (
-                <CldImage
-                  width={"500"}
-                  height={"500"}
-                  src={imageId}
-                  sizes="100vw"
-                  alt="Photo of the product"
-                />
-              )}
-
-              {imageId !== "" || (
-                <Image
-                  src={"/images/placeholder-image.webp"}
-                  alt="Product placeholder image"
-                  width={1200}
-                  height={800}
-                  className="w-full"
-                />
-              )}
-
-              <p>Image of the product</p>
             </div>
 
-            {/* Is Featured */}
-            <FormField
-              control={form.control}
-              name="isFeatured"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
-                  <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                  <div className="space-y-1 leading-none">
-                    <FormLabel>Feature Product</FormLabel>
-                    <FormDescription>
-                      Check this box to set this product as a &#34;Featured
-                      Product&#34;
-                    </FormDescription>
-                  </div>
-                </FormItem>
-              )}
-            />
-          </div>
+            <p className="text-sm text-muted-foreground">
+              Image of the product
+            </p>
+          </section>
         </section>
+
+        {/* Product Quick Overview */}
+        <FormField
+          control={form.control}
+          name="quickOverview"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Quick Overview (Optional)</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Write something short about the product..."
+                  {...field}
+                  rows={5}
+                  autoComplete="off"
+                />
+              </FormControl>
+              <FormDescription>
+                Write short description about the product.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Product Discount */}
+        <FormField
+          control={form.control}
+          name="discount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Discount (Optional)</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  placeholder="40%"
+                  {...field}
+                  max={100}
+                  autoComplete="off"
+                />
+              </FormControl>
+              <FormDescription>
+                Product discount amount in percentage.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Product Description */}
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Description (Optional)</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Write something about the product..."
+                  {...field}
+                  rows={10}
+                  autoComplete="off"
+                />
+              </FormControl>
+              <FormDescription>
+                Write useful description about the product.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
         <Button type="submit" disabled={isLoading}>
           {isLoading ? (
@@ -298,7 +256,7 @@ const EditProductForm = ({
               Saving...
             </>
           ) : (
-            <>Save Edit</>
+            <>Save</>
           )}
         </Button>
       </form>
