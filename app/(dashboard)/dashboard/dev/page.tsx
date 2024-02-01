8e211073f26d1a1f8d89c51f3e51efde39eb9291
Rifat Khan <rifat6633@gmail.com>
2024-02-01 18:09:22 +0600

    create: dashboard dev page

---
+++ app/(dashboard)/dashboard/dev/page.tsx
@@ -0,0 +1,19 @@
+import Title from "@/components/uis/title";
+
+const DevPage = () => {
+  return (
+    <>
+      <section>
+        <Title variant={"xl"}>Dev Environment</Title>
+      </section>
+
+      <section className="space-y-5">
+        <section>
+          <h1>Create Dummy Product</h1>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default DevPage;
