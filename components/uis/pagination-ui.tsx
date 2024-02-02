7f0bda494c10e9b5b66f82fb1926d279ae17753d
Rifat Khan <rifat6633@gmail.com>
2024-02-02 22:35:12 +0600

    create: PaginationUi for all pragination

---
+++ components/uis/pagination-ui.tsx
@@ -0,0 +1,181 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+const PaginationUi = ({
+  pages,
+  currentPage,
+}: {
+  pages: number;
+  currentPage: number;
+}) => {
+  const pathname = usePathname();
+
+  const pagesArr = Array.from({ length: pages }, (_, i) => i + 1);
+
+  return (
+    <section className="py-10">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem className="hover:border hover:border-muted">
+            <PaginationPrevious
+              href={`${pathname}?page=${
+                currentPage !== 1 ? currentPage - 1 : 1
+              }`}
+            />
+          </PaginationItem>
+
+          {/* When number of page is equal or less than 5 (page <= 5) */}
+          {pages <= 5 &&
+            pagesArr.map((list) => (
+              <PaginationItem key={list}>
+                <PaginationLink
+                  href={`/shop?page=${list}`}
+                  isActive={list === currentPage ? true : false}
+                >
+                  {list}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+          {/* When number of page are greater than 5 (page > 5) */}
+          {pages > 5 && (
+            <>
+              <PaginationPage
+                page={1}
+                isActive={currentPage === 1 ? true : false}
+              />
+
+              {/* If current page is 4 or greater than 4 put an ellipsis. (currentPage >= 4) */}
+              {currentPage >= 4 && (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+
+              {/* When number of pages is 6. (pages === 6 && currentPage <= 3) */}
+              {pages === 6 ? (
+                <>
+                  {/* If current page is 3 or less then 3. (currentPage <= 3) */}
+                  {currentPage <= 3 ? (
+                    <>
+                      <PaginationPage
+                        page={2}
+                        isActive={currentPage === 2 ? true : false}
+                      />
+                      <PaginationPage
+                        page={3}
+                        isActive={currentPage === 3 ? true : false}
+                      />
+                      <PaginationPage page={4} />
+                    </>
+                  ) : (
+                    <>
+                      <PaginationPage page={3} />
+                      <PaginationPage
+                        page={4}
+                        isActive={currentPage === 4 ? true : false}
+                      />
+                      <PaginationPage
+                        page={5}
+                        isActive={currentPage === 5 ? true : false}
+                      />
+                    </>
+                  )}
+                </>
+              ) : (
+                <>
+                  {/* If current page is 3 or less then 3. (currentPage <= 3) */}
+                  {currentPage <= 3 ? (
+                    <>
+                      <PaginationPage
+                        page={2}
+                        isActive={currentPage === 2 ? true : false}
+                      />
+                      <PaginationPage
+                        page={3}
+                        isActive={currentPage === 3 ? true : false}
+                      />
+                      <PaginationPage page={4} />
+                    </>
+                  ) : (
+                    <>
+                      {currentPage >= pages - 2 ? (
+                        <>
+                          <PaginationPage page={pages - 3} />
+                          <PaginationPage
+                            page={pages - 2}
+                            isActive={currentPage === pages - 2 ? true : false}
+                          />
+                          <PaginationPage
+                            page={pages - 1}
+                            isActive={currentPage === pages - 1 ? true : false}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          <PaginationPage page={currentPage - 1} />
+                          <PaginationPage page={currentPage} isActive={true} />
+                          <PaginationPage page={currentPage + 1} />
+                        </>
+                      )}
+                    </>
+                  )}
+                </>
+              )}
+
+              {/* If current page is (pages - 3) or less than (pages - 3) put an ellipsis. (currentPage <= pages - 3) */}
+              {currentPage <= pages - 3 && (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+
+              <PaginationPage
+                page={pages}
+                isActive={currentPage === pages ? true : false}
+              />
+            </>
+          )}
+
+          <PaginationItem>
+            <PaginationNext
+              href={`${pathname}?page=${
+                currentPage !== pages ? currentPage + 1 : pages
+              }`}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </section>
+  );
+};
+
+const PaginationPage = ({
+  page,
+  isActive,
+}: {
+  page: string | number;
+  isActive?: boolean;
+}) => {
+  const pathname = usePathname();
+
+  return (
+    <PaginationItem>
+      <PaginationLink href={`${pathname}?page=${page}`} isActive={isActive}>
+        {page}
+      </PaginationLink>
+    </PaginationItem>
+  );
+};
+
+export default PaginationUi;
