09b08d8be55ebd385ab56af5ee7cc862f4a7ae41
Rifat Khan <rifat6633@gmail.com>
2024-02-08 11:58:41 +0600

    update: address table

--- app/(profile)/profile/_parts/user-address.tsx
+++ app/(profile)/profile/_parts/user-address.tsx
@@ -6,24 +6,51 @@ const UserAddress = async () => {
   const address = await getUserAddress();
 
   return (
-    <section>
+    <section className="space-y-2">
       {address ? (
         <>
+          <div>
+            <OrderInfoField
+              field={"Delivary address"}
+              value={address?.address}
+            />
+            <OrderInfoField field={"Zip code"} value={address?.zipCode} />
+            <OrderInfoField field={"City"} value={address?.city} />
+            <OrderInfoField
+              field={"State or Country"}
+              value={address?.country}
+            />
+          </div>
           <Button variant={"secondary"} asChild>
             <Link href={`/profile/address`}>Edit your address</Link>
           </Button>
         </>
       ) : (
-        <div className="space-y-2">
+        <>
           <h1 className="text-muted-foreground">
             You have not set any address
           </h1>
           <Button variant={"secondary"} asChild>
             <Link href={`/profile/address`}>Add your address</Link>
           </Button>
-        </div>
+        </>
       )}
     </section>
+  );
+};
+
+const OrderInfoField = ({
+  field,
+  value,
+}: {
+  field?: string;
+  value?: string;
+}) => {
+  return (
+    <p className="grid grid-cols-12 gap-1 p-1 px-2">
+      <span className="col-span-4 text-muted-foreground">{field}</span>{" "}
+      <span className="text-foreground col-span-8">{value}</span>
+    </p>
   );
 };
 
