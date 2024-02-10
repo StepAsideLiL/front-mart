56bcdc59c143bf8d5907a2265753fcf7e694cf48
Rifat Khan <rifat6633@gmail.com>
2024-02-10 10:25:48 +0600

    change: link title

--- app/(profile)/profile/_parts/user-order.tsx
+++ app/(profile)/profile/_parts/user-order.tsx
@@ -14,7 +14,7 @@ const UserOrder = async () => {
             Total orders: <Badge variant={"secondary"}>{orders?.length}</Badge>
           </h1>
           <Button variant={"secondary"} asChild>
-            <Link href={`/profile/wishlist`}>View wish list</Link>
+            <Link href={`/profile/wishlist`}>View orders</Link>
           </Button>
         </>
       ) : (
