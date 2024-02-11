814869529fffac9a47f0de814648cd8e4cf62233
Rifat Khan <rifat6633@gmail.com>
2024-02-11 10:40:09 +0600

    create: dashboard users table

---
+++ app/(dashboard)/dashboard/users/_parts/user-list.tsx
@@ -0,0 +1,67 @@
+import { clerkClient } from "@clerk/nextjs";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+
+const UserList = async () => {
+  const users = await clerkClient.users.getUserList({
+    orderBy: "+created_at",
+  });
+
+  return (
+    <section>
+      {users && users.length !== 0 ? (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-14"></TableHead>
+              <TableHead>Username</TableHead>
+              <TableHead>Role</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>
+                  <Link href={``}>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={user.imageUrl}
+                      alt={`Profile picture of ${user.username}`}
+                      className="aspect-square"
+                    />
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Link href={``} className="hover:underline">
+                    {user.username}
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  {user.publicMetadata.role
+                    ? (user.publicMetadata.role as string)
+                    : "user"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <div className="p-10">
+          <h1 className="text-muted-foreground text-xl text-center">
+            No users
+          </h1>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default UserList;
--- app/(dashboard)/dashboard/users/page.tsx
+++ app/(dashboard)/dashboard/users/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Title from "@/components/uis/title";
+import { Suspense } from "react";
+import UserList from "./_parts/user-list";
 
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <>
+      <Title variant={"xl"}>Users</Title>
+
+      <Suspense fallback={"loading..."}>
+        <UserList />
+      </Suspense>
+    </>
+  );
 };
 
 export default UsersPage;
