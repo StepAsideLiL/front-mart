bd3b72574b531813389aedf0e9e2b93925ad3e4d
Rifat Khan <rifat6633@gmail.com>
2024-02-12 20:14:17 +0600

    create: features section

---
+++ app/(web)/_parts/web-app-features.tsx
@@ -0,0 +1,21 @@
+const WebAppFeatures = () => {
+  return (
+    <section className="space-y-10">
+      <h1 className="text-4xl font-semibold text-center">
+        Features Of The E-Store
+      </h1>
+
+      <div className="w-60 mx-auto space-y-2">
+        <p>Light and Dark Mode</p>
+        <p>
+          Product cart and cart is stored in the browser&apos;s local storage
+        </p>
+        <p>Add product form</p>
+        <p>Admin dashboard for products and orders list</p>
+        <p>User profile and Product wish list</p>
+      </div>
+    </section>
+  );
+};
+
+export default WebAppFeatures;
