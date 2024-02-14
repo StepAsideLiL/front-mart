4bd5fc487fdc721ca216db166c2ac9b79a0df130
Rifat Khan <rifat6633@gmail.com>
2024-02-14 13:28:27 +0600

    update: increase top and bottom padding

--- components/templates/footer.tsx
+++ components/templates/footer.tsx
@@ -6,7 +6,7 @@ const year = date.getFullYear();
 const Footer = () => {
   return (
     <footer>
-      <div className="flex items-center justify-center px-6 py-6 text-sm">
+      <div className="flex items-center justify-center px-6 py-10 text-sm">
         <span className="dark:text-gray-400">
           © Copyright {year}. All Rights Reserved{" "}
           <Link
