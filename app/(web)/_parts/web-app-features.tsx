ab34acdbac7895136665519560b0ca638f2e5c81
Rifat Khan <rifat6633@gmail.com>
2024-02-14 20:38:36 +0600

    update: features text style

--- app/(web)/_parts/web-app-features.tsx
+++ app/(web)/_parts/web-app-features.tsx
@@ -1,3 +1,6 @@
+import { Cd } from "@/lib/types";
+import { Check } from "lucide-react";
+
 const WebAppFeatures = () => {
   return (
     <section className="space-y-10">
@@ -5,16 +8,24 @@ const WebAppFeatures = () => {
         Features Of The E-Store
       </h1>
 
-      <div className="w-60 mx-auto space-y-2">
-        <p>Light and Dark Mode</p>
-        <p>
+      <div className="w-60 mx-auto space-y-2 font-mono">
+        <FeatureText>Light and Dark Mode</FeatureText>
+        <FeatureText>
           Product cart and cart is stored in the browser&apos;s local storage
-        </p>
-        <p>Add product form</p>
-        <p>Admin dashboard for products and orders list</p>
-        <p>User profile and Product wish list</p>
+        </FeatureText>
+        <FeatureText>Add product form</FeatureText>
+        <FeatureText>Admin dashboard for products and orders list</FeatureText>
+        <FeatureText>User profile and Product wish list</FeatureText>
       </div>
     </section>
+  );
+};
+
+const FeatureText = ({ children }: Cd) => {
+  return (
+    <p className="font-mono flex gap-0.5 w-full items-start">
+      <Check size={"16"} className="min-w-5 mt-1" /> <span>{children}</span>
+    </p>
   );
 };
 
