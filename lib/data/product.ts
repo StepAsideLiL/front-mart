eac488cd97511a0038af1ccd39cedec15bafc940
Rifat Khan <rifat6633@gmail.com>
2024-06-29 18:31:29 +0600

    refactor: move calculateCartPrice function to product object

--- lib/data/product.ts
+++ lib/data/product.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prismadb";
 import { unstable_noStore } from "next/cache";
+import { calculateDiscountedPrice } from "@/lib/utils";
 
 const productsPerPage = 10;
 
@@ -37,7 +38,38 @@ const totalPageForProduct = async () => {
   }
 };
 
+// Calacute the total price of the cart products on the chcckout page.
+const calculateCartPrice = async (products: { id: string }[]) => {
+  unstable_noStore();
+
+  try {
+    let price = 0;
+    await Promise.all(
+      products.map(async (item) => {
+        const product = await prisma.product.findUnique({
+          where: {
+            id: item.id,
+          },
+        });
+
+        if (product!.discount === 0) {
+          price = price + product!.price;
+        } else {
+          price =
+            price + calculateDiscountedPrice(product!.price, product!.discount);
+        }
+      })
+    );
+
+    return price;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Could not calculate total price.");
+  }
+};
+
 export const product = {
   getProducts,
   totalPageForProduct,
+  calculateCartPrice,
 };
