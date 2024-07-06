9d16cbccb1a74b42cfca6fb40a9962ab237011c5
Rifat Khan <rifat6633@gmail.com>
2024-07-06 17:17:06 +0600

    refactor: remove link

--- app/(dashboard)/dashboard/products/_parts/add-product-btn.tsx
+++ app/(dashboard)/dashboard/products/_parts/add-product-btn.tsx
@@ -1,14 +1,13 @@
+"use server";
+
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
-import Link from "next/link";
 
 export default function AddProductBtn() {
   return (
-    <Button asChild variant={"outline"}>
-      <Link href={"/product/add"} className="flex items-center gap-1">
-        <Plus size={20} />
-        <span>Add Product</span>
-      </Link>
+    <Button variant={"outline"}>
+      <Plus size={20} />
+      <span>Add Product</span>
     </Button>
   );
 }
