4f7fe8345b1edf9676eff4c302a0c810626a65d4
Rifat Khan <rifat6633@gmail.com>
2024-07-06 18:10:21 +0600

    update: add createProductOnDatabase server action

--- app/(dashboard)/dashboard/products/_parts/add-product-btn.tsx
+++ app/(dashboard)/dashboard/products/_parts/add-product-btn.tsx
@@ -1,11 +1,15 @@
 "use server";
 
 import { Button } from "@/components/ui/button";
+import { product } from "@/lib/data";
 import { Plus } from "lucide-react";
 
 export default function AddProductBtn() {
   return (
-    <Button variant={"outline"}>
+    <Button
+      variant={"outline"}
+      onClick={() => product.createProductOnDatabase()}
+    >
       <Plus size={20} />
       <span>Add Product</span>
     </Button>
