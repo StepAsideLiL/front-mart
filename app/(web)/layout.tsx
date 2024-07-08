9e7702d39f7443ea2e619bf26d00de7b24149851
Rifat Khan <rifat6633@gmail.com>
2024-07-08 21:40:43 +0600

    add: header and footer

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -1,16 +1,52 @@
-import Footer from "@/components/templates/footer";
-import Navbar from "@/components/templates/navbar";
+import Cart from "@/components/templates/cart";
+import { ModeToggle } from "@/components/templates/mode-toggle";
+import Navlink from "@/components/templates/navlink";
+import { Separator } from "@/components/ui/separator";
+import Logo from "@/components/uis/logo";
+import Link from "next/link";
 
-const WebLayout = ({ children }: { children: React.ReactNode }) => {
+const date = new Date();
+const year = date.getFullYear();
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <Navbar />
+    <>
+      <header className="container py-5 flex justify-between items-center">
+        <nav className="flex items-center gap-3 h-8">
+          <Logo variant="link" />
+
+          <Separator orientation="vertical" />
+
+          <Navlink />
+        </nav>
+
+        <div className="flex items-center gap-3 h-8">
+          <ModeToggle />
+
+          <Separator orientation="vertical" />
+
+          <Cart />
+        </div>
+      </header>
 
       {children}
 
-      <Footer />
-    </div>
+      <footer>
+        <div className="flex items-center justify-center px-6 py-10 text-sm">
+          <span className="dark:text-gray-400">
+            © Copyright {year}. All Rights Reserved{" "}
+            <Link
+              href={"https://github.com/stepasidelil"}
+              target="_blank"
+              className="underline"
+            >
+              Rifat Khan
+            </Link>
+          </span>
+        </div>
+      </footer>
+    </>
   );
 };
 
-export default WebLayout;
+export default Layout;
