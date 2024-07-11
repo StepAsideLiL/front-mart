823cb3fd7eb42dae300021f86d61ea45d29019a2
Rifat Khan <rifat6633@gmail.com>
2024-07-11 12:37:11 +0600

    update: user profile edit page

--- app/(profile)/profile/edit/[[...edit]]/page.tsx
+++ app/(profile)/profile/edit/[[...edit]]/page.tsx
@@ -1,14 +1,19 @@
-import { UserProfile } from "@clerk/nextjs";
+import EditProfileForm from "@/components/app-components/profile/forms/edit-profile";
+import Title from "@/components/uis/title";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "User Profile",
 };
 
-export default function SignUpPage() {
+export default function Page() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <UserProfile path="/profile/edit" routing="path" />
+    <main className="min-h-screen container py-5 space-y-10">
+      <section>
+        <Title variant={"xl2"}>Edit Profile</Title>
+      </section>
+
+      <EditProfileForm />
     </main>
   );
 }
