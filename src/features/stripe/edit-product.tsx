6361eeaf91069395978aca0edfc311343630abe8
Rifat Khan <rifat6633@gmail.com>
2024-07-13 22:34:31 +0600

    update: turn edit-product to an async component

--- src/features/stripe/edit-product.tsx
+++ src/features/stripe/edit-product.tsx
@@ -1,24 +1,29 @@
 import { Separator } from "@/components/ui/separator";
 import GeneralInfoForm from "./forms/general-info";
+import { product } from "@/lib/data";
 
-export default function StripeProductEditApp({
+export default async function EditProduct({
   productId,
-  productTitle,
-  description,
-  quickDescription,
 }: {
   productId: string;
-  productTitle: string;
-  description: string;
-  quickDescription: string;
 }) {
+  const productInfo = await product.getProductsInfoForEdit(productId);
+
+  if (!productInfo) {
+    return (
+      <div className="flex justify-center items-center h-96">
+        <h1 className="text-2xl text-muted-foreground">No Product Found</h1>
+      </div>
+    );
+  }
+
   return (
     <section className="space-y-8">
       <GeneralInfoForm
         productId={productId}
-        productTitle={productTitle}
-        description={description}
-        quickDescription={quickDescription}
+        productTitle={productInfo.productTitle}
+        description={productInfo.description}
+        quickDescription={productInfo.quickDescription}
       />
 
       <Separator />
