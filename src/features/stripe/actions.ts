cb822f16268519a9a75b8105419f722fcdbd4a20
Rifat Khan <rifat6633@gmail.com>
2024-07-15 17:35:17 +0600

    create: updateProductVariantSchema server action

--- src/features/stripe/actions.ts
+++ src/features/stripe/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prismadb";
+import { json } from "stream/consumers";
 
 /**
  * Update the general information of a product.
@@ -33,5 +34,36 @@ export async function updateGeneralProductInfo(data: {
   } catch (error) {
     console.log(error);
     throw new Error("Failed to perform updateProductInfo server action.");
+  }
+}
+
+/**
+ * Update variant schema of a product.
+ * @param productId Product ID from the database.
+ * @param productVariantSchema Updated variant Schema of the product after stringify the JSON.
+ * @returns Object with success boolean and message string
+ */
+export async function updateProductVariantSchema(
+  productId: string,
+  productVariantSchema: string
+) {
+  try {
+    const updatedProduct = await prisma.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        variantSchema: productVariantSchema,
+      },
+    });
+
+    if (updatedProduct) {
+      return { success: true, message: "Variant updated successfully!" };
+    } else {
+      return { success: false, message: "Failed to update product." };
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to perform updateProductVariants server action");
   }
 }
