57b75b4c304c2c78907bc0819e490c914e76e22d
Rifat Khan <rifat6633@gmail.com>
2024-07-18 19:39:20 +0600

    create: edit-product-accordion

---
+++ src/features/stripe/ui/edit-product-accordion.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+const EditProductAccordion = AccordionPrimitive.Root;
+
+const EditProductAccordionItem = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={cn("border", className)}
+    {...props}
+  />
+));
+EditProductAccordionItem.displayName = "AccordionItem";
+
+const EditProductAccordionTrigger = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        "px-5 py-3 bg-muted flex flex-1 items-center justify-between font-medium transition-all [&[data-state=open]>svg]:rotate-180",
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+));
+EditProductAccordionTrigger.displayName =
+  AccordionPrimitive.Trigger.displayName;
+
+const EditProductAccordionContent = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Content
+    ref={ref}
+    className="px-5 overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    {...props}
+  >
+    <div className={cn("pt-0", className)}>{children}</div>
+  </AccordionPrimitive.Content>
+));
+
+EditProductAccordionContent.displayName =
+  AccordionPrimitive.Content.displayName;
+
+export {
+  EditProductAccordion,
+  EditProductAccordionItem,
+  EditProductAccordionTrigger,
+  EditProductAccordionContent,
+};
